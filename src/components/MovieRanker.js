4a6e00c20d35746caf5e1f51f1260cae977d3a2b
Nicolas Buisson <nicolas.buisson@mail.mcgill.ca>
2020-12-24 19:08:09 +0100

    converted from react class component to functional

--- src/components/MovieRanker.js
+++ src/components/MovieRanker.js
@@ -1,142 +1,132 @@
-import React, { Component } from "react";
+import React, { Component, useEffect, useState } from "react";
 import "../styles/MovieRanker.css";
 
-export class MovieRanker extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      searchTerm: "",
-      foundMovies: ["Rambo", "Bambi", "Mulan"],
-      nominatedMovies: ["Pirates of the Carribean", "Shrek"],
-    };
+const MovieRanker = () => {
+  const [searchTerm, setSearchTerm] = useState("");
+  const [foundMovies, setFoundMovies] = useState(["Carribean", "Ursula"]);
+  const [nominatedMovies, setNominatedMovies] = useState([
+    "Pirates of the Carribean",
+    "Shrek",
+  ]);
 
-    //functions
-    this.checkNominatedMoviesSize = this.checkNominatedMoviesSize.bind(this);
-    this.updateSearchTerm = this.updateSearchTerm.bind(this);
-    this.removeNomination = this.removeNomination.bind(this);
-    this.nominateMovie = this.nominateMovie.bind(this);
-    this.searchMovies = this.searchMovies.bind(this);
-  }
-
-  checkNominatedMoviesSize() {
-    if (this.state.nominatedMovies.length >= 5) {
+  const checkNominatedMoviesSize = () => {
+    if (nominatedMovies.length >= 5) {
       alert("Movie Nomination Complete");
     }
     /*this works
     do a "Banner"
     not sure what this means, a pop-up?
     a div at the bottom that appears to say it's complete?*/
-  }
+  };
 
-  updateSearchTerm(event) {
-    this.setState({ searchTerm: event.target.value });
-  }
+  const updateSearchTerm = (event) => {
+    setSearchTerm(event.target.value);
+  };
 
-  removeNomination(event) {
-    let index = this.state.nominatedMovies.indexOf(event.target.value);
-    this.state.nominatedMovies.splice(index, 1);
-    this.setState({ nominatedMovies: this.state.nominatedMovies });
-  }
+  const removeNomination = (event) => {
+    let index = nominatedMovies.indexOf(event.target.value);
+    nominatedMovies.splice(index, 1);
+    let newNominatedMovies = [...nominatedMovies];
+    setNominatedMovies(newNominatedMovies);
+  };
 
-  nominateMovie(event) {
-    let newNominatedMovies = [
-      ...this.state.nominatedMovies,
-      event.target.value,
-    ];
-    this.setState({ nominatedMovies: newNominatedMovies });
-  }
+  const nominateMovie = (event) => {
+    let newNominatedMovies = [...nominatedMovies, event.target.value];
+    setNominatedMovies(newNominatedMovies);
+  };
 
-  searchMovies() {
-    //perform API call
-    let url = "http://www.omdbapi.com/?apikey=7409b0fc&&s=shrek"; //+ this.state.searchTerm; //type=movie
-    console.log(url);
-    fetch(url)
-      .then((response) => {
-        response.json();
-        // console.log(response);
-      })
-      .then((movies) => {
-        console.log("movies: " + movies);
-      })
-      .catch((error) => {
-        console.log(error);
-      });
-  }
+  const searchMovies = async () => {
+    let url =
+      "http://www.omdbapi.com/?apikey=7409b0fc&type=movie&s=" + searchTerm;
 
-  render() {
-    //this.checkNominatedMoviesSize();
-    this.searchMovies();
-    return (
-      <div className="container">
-        <h1 className="main-title">The Shoppies</h1>
-        <p className="main-para">
-          Choose 5 movies for awards. <strong>You</strong> decide.
-        </p>
-        <div className="grid-container">
-          <div className="search-container">
-            <h2 className="search-subtitle">Movie title</h2>
-            <div className="search-bar">
-              {/* add search icon */}
-              <input
-                className="search-bar-input"
-                type="text"
-                value={this.state.searchTerm}
-                onChange={this.updateSearchTerm}
-              ></input>
-            </div>
-          </div>
+    let response = await fetch(url);
+    let responseJson = await response.json();
 
-          <div className="foundMovies-container">
-            <h2 className="subtitle">Results for "{this.state.searchTerm}"</h2>
-            {/** add movie/film icon */}
-            <ul className="foundMovies-list">
-              {this.state.foundMovies.map(
-                //change this part when u add in Backend call
-                (foundMovie) => (
-                  // console.log(foundMovie),
-                  <li className="foundMovies-list-item">
-                    <div className="foundMovies-list-label">{foundMovie}</div>
-                    <button
-                      className="foundMovies-list-button"
-                      onClick={this.nominateMovie}
-                      value={foundMovie}
-                      disabled={this.state.nominatedMovies.includes(foundMovie)}
-                    >
-                      Nominate
-                    </button>
-                  </li>
-                )
-              )}
-            </ul>
-          </div>
+    console.log(responseJson.Search);
+    let moviesTitleYear = [];
+    if (responseJson.Search !== undefined) {
+      for (let i = 0; i < responseJson.Search.length; i++) {
+        if (i === 7) {
+          break;
+        }
+        moviesTitleYear.push(
+          responseJson.Search[i].Title +
+            " (" +
+            responseJson.Search[i].Year +
+            ")"
+        );
+      }
+      console.log(moviesTitleYear);
+      setFoundMovies(moviesTitleYear);
+    }
+  };
 
-          <div className="nominatedMovies-container">
-            <h2 className="subtitle">Nominations</h2>
-            {/**add trophy/medal icon, gold color */}
-            <ul className="nominatedMovies-list">
-              {this.state.nominatedMovies.map(
-                //change this part when u add in Backend call
-                (nominatedMovie) => (
-                  // console.log(nominatedMovie),
-                  <li className="nominatedMovies-list-item">
-                    <div className="nominatedMovies-list-label">
-                      {nominatedMovie}
-                    </div>
-                    <button
-                      className="nominatedMovies-list-button"
-                      onClick={this.removeNomination}
-                      value={nominatedMovie}
-                    >
-                      Remove
-                    </button>
-                  </li>
-                )
-              )}
-            </ul>
+  useEffect(() => {
+    searchMovies();
+  });
+
+  return (
+    <div className="container">
+      <h1 className="main-title">The Shoppies</h1>
+      <p className="main-para">
+        Choose 5 movies for awards. <strong>You</strong> decide.
+      </p>
+      <div className="grid-container">
+        <div className="search-container">
+          <div className="search-bar">
+            {/* add search icon */}
+            <input
+              className="search-bar-input"
+              type="text"
+              placeholder="Search by movie title"
+              value={searchTerm}
+              onChange={updateSearchTerm}
+            ></input>
           </div>
         </div>
+
+        <div className="foundMovies-container">
+          <h2 className="subtitle">Results for "{searchTerm}"</h2>
+          {/** add movie/film icon */}
+          <ul className="foundMovies-list">
+            {foundMovies.map((foundMovie) => (
+              <li className="foundMovies-list-item">
+                <div className="foundMovies-list-label">{foundMovie}</div>
+                <button
+                  className="foundMovies-list-button"
+                  onClick={nominateMovie}
+                  value={foundMovie}
+                  disabled={nominatedMovies.includes(foundMovie)}
+                >
+                  Nominate
+                </button>
+              </li>
+            ))}
+          </ul>
+        </div>
+
+        <div className="nominatedMovies-container">
+          <h2 className="subtitle">Nominations</h2>
+          {/**add trophy/medal icon, gold color */}
+          <ul className="nominatedMovies-list">
+            {nominatedMovies.map((nominatedMovie) => (
+              <li className="nominatedMovies-list-item">
+                <div className="nominatedMovies-list-label">
+                  {nominatedMovie}
+                </div>
+                <button
+                  className="nominatedMovies-list-button"
+                  onClick={removeNomination}
+                  value={nominatedMovie}
+                >
+                  Remove
+                </button>
+              </li>
+            ))}
+          </ul>
+        </div>
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
 export default MovieRanker;
